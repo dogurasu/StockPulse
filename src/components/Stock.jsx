b9b88b5f442bb9043dc44911c6e6294edd83ab50
dogurasu <dugs134@gmail.com>
2020-10-16 16:10:12 -0700

    refactored Stock from Class to Functional component

--- src/components/Stock.jsx
+++ src/components/Stock.jsx
@@ -1,85 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Plot from 'react-plotly.js';
+import axios from 'axios';
+// import axios from '../apis/alphavantage';
 
-const path = require('path');
-// console.log(path.isAbsolute(path.join(__dirname, 'apis')));
-console.log(path.join(__dirname, ''));
+const Stock = (props) => {
+    const [chartXVals, setChartXVals] = useState([]);
+    const [chartYVals, setChartYVals] = useState([]);
+    const [ticker, setTicker] = useState('');
 
-class Stock extends React.Component {
-    // constructor to store our state
-    constructor(props) {
-        super(props);
-        this.state = {
-            stockChartXVals: [],
-            stockChartYVals: []
-        }
-    }
+    useEffect(() => {
+        // let api_call = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=FB&outputsize=compact&apikey=${process.env.REACT_APP_API_KEY_AV}`;
+
+        try {
+            const getCoordinates = async () => {
+                const func = "TIME_SERIES_INTRADAY";
+                const res = await axios.get(`https://www.alphavantage.co/query?function=${func}`, {
+                    params: {
+                        symbol: "FB",
+                        interval: "1min",
+                        outputsize: "compact",
+                        apikey: process.env.REACT_APP_API_KEY_AV
+                    }
+                });
+                console.log(res);
     
-    fetchStock = () => {
-        const pointerToThis = this;
-        console.log(pointerToThis);
-        const API_KEY_AV = process.env.API_KEY;
-        let api_call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=FB&outputsize=compact&apikey=${API_KEY_AV}`;
-        let stockChartXValuesFunction = [];
-        let stockChartYValuesFunction = [];
-
-        fetch(api_call)
-            .then((res, rej) => {
-                return res.json();
-            })
-            .then((json) => {
-                console.log(json);
-
-                for (let key in json["Time Series (Daily)"]) {
-                    stockChartXValuesFunction.push(key);
-                    stockChartYValuesFunction.push(json["Time Series (Daily)"][key]["1. open"]);
+                let x = [];
+                let y = [];
+    
+                for (let key in res.data["Time Series (1min)"]) {
+                    x.push(key);
+                    y.push(res.data["Time Series (1min)"][key]["1. open"]);
                 }
-
-                // console.log(stockChartXValuesFunction);
-                pointerToThis.setState({
-                    stockChartXVals: stockChartXValuesFunction,
-                    stockChartYVals: stockChartYValuesFunction,
-                })
-            })
-    }
-
-    componentDidMount = () => {
-        this.fetchStock();
-    }
-
     
+                // console.log(x.length);
+                // console.log(y.length);
+    
+                setChartXVals(x);
+                setChartYVals(y);
+            }
+    
+            // console.log("API KEY: " + process.env.REACT_APP_API_KEY_AV)
+            
+            getCoordinates();
 
-    render() {
-        return (
-            <div>
-                <h1>Stock Market</h1>
-                {/* <p>x-values: {this.state.stockChartXValues}</p>
-                <p>y-values: {this.state.stockChartYValues}</p> */}
-                <Plot
-                    data={[
-                        {
-                            x: this.state.stockChartXVals,
-                            y: this.state.stockChartYVals,
-                            type: 'scatter',
-                            mode: 'lines+markers',
-                            marker: {color: 'red'},
-                        },
-                    ]}
-                    layout={{
-                        width: 720,
-                        height: 420,
-                        title: "A Fancy Plot",
-                        xaxis: {
-                            title: "Date"
-                        },
-                        yaxis: {
-                            title: "Price"
-                        }
-                    }}
-                />
-            </div>
-        )
-    }
+        } catch(err) {
+            console.log(err);
+        }
+    }, []);
+
+    return (
+        <div>
+            <h1>Stock Market</h1>
+            {/* <p>x-values: {this.state.stockChartXValues}</p>
+            <p>y-values: {this.state.stockChartYValues}</p> */}
+            <Plot
+                data={[
+                    {
+                        x: chartXVals,
+                        y: chartYVals,
+                        type: 'scatter',
+                        mode: 'lines+markers',
+                        marker: {color: 'red'},
+                    },
+                ]}
+                layout={{
+                    width: 720,
+                    height: 420,
+                    title: "A Fancy Plot",
+                    xaxis: {
+                        title: "Date"
+                    },
+                    yaxis: {
+                        title: "Price"
+                    }
+                }}
+            />
+            <input type="text" id="ticker_input"/>
+            
+            {/* <button onClick={this.changeStateString.bind(this)} style={{padding: "1rem", fontSize: "24px", fontWeight: "500"}}>Change State!</button> */}
+            {/* style={{width: "100px", height: "100px"}} */}
+        </div>
+    );
 }
 
 export default Stock;
