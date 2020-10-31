e11088abdf4257da6e5f57977681eb08054d1615
dogurasu <dugs134@gmail.com>
2020-10-30 23:58:21 -0700

    added scss import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import 'semantic-ui-css/semantic.min.css';
+import './scss/index.scss';
 
 ReactDOM.render(<App />, document.querySelector("#root"));
